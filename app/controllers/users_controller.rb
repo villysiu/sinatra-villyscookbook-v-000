004aba8608e2331195463d8c8ea1b986fbfcc246
Villy Siu <villysiu@gmail.com>
2017-08-04 21:57:04 +0000

    setup controller user setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,23 @@
-=class UsersController < ApplicationController
+class UsersController < ApplicationController
+
   get 'users/login' do
     redirect '/'
   end
+
 
   post '/users/login' do
     @current_user = User.find_by(:name => params[:name])
 
     session[:id] = @current_user.id
     redirect '/recipes'
+  end
+
+  get 'users/signup' do
+    erb :'/users/signup'
+  end
+
+  post '/users/signup' do
+    @user = User.create(params[:user])
+    redirect "/recipes"
   end
 end
