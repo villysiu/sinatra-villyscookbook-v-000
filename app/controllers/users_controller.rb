55f7e19269dcacf04694a24008ec14529057fc02
Villy Siu <villysiu@gmail.com>
2017-08-04 21:42:45 +0000

    signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
-class UsersController < ApplicationController
+=class UsersController < ApplicationController
   get 'users/login' do
     redirect '/'
   end
 
   post '/users/login' do
+    @current_user = User.find_by(:name => params[:name])
 
+    session[:id] = @current_user.id
+    redirect '/recipes'
   end
 end
