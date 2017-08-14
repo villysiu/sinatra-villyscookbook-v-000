84e1ce62d0903bfe45c588aba1a0d5e042c7620b
Villy Siu <villysiu@gmail.com>
2017-08-14 17:50:46 +0000

    post route to create new recipe object

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,4 @@
-class RecipesController < ApplicationController
+ class RecipesController < ApplicationController
   get '/recipes' do
     if logged_in?
       @recipes = Recipe.all
@@ -8,6 +8,7 @@ class RecipesController < ApplicationController
       redirect "/"
     end
   end
+
   get '/recipes/new' do
     if logged_in?
       erb :'/recipes/create_recipe'
@@ -18,7 +19,22 @@ class RecipesController < ApplicationController
   end
 
   post '/recipes/new' do
+    @recipe = Recipe.new(params[:recipe])
+    @recipe.user = current_user
 
+    if @recipe.valid?
+      params[:ingredient].each do |i|
+        if !i[:name].empty?
+          @recipe.ingredients << i.find_or_create(name: i[:name])
+        end
+      end
+      @recipe.save
+      current_user.recipes << @recipe
+      flash[:message] = "#{@recipe.name} successfully created"
+      redirect "/recipes/#{@recipe.id}"
+    else
+      erb :'/recipes/create_recipe'
+    end
   end
 
 end
