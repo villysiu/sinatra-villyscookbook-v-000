543eedcb4599ee5393351822b4cb7d5495cfa306
Villy Siu <villysiu@gmail.com>
2017-08-14 16:55:56 +0000

    create @recipes to collect all recipe to use in show_all_recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,11 @@
 class RecipesController < ApplicationController
   get '/recipes' do
-    erb :'/recipes/show_all_recipes'
+    if logged_in?
+      @recipes = Recipe.all
+      erb :'/recipes/show_all_recipes'
+    else
+      flash[:message] = "You are not logged in"
+      redirect "/"
+    end
   end
 end
